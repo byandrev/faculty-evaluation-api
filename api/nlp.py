001f0d736eed4070053806a8167eed3e80a0d370
Andres Parra <byandrev@gmail.com>
2026-01-24 16:49:42 -0500

    feat: add danger analysis to comments using a new Hugging Faca

--- api/nlp.py
+++ api/nlp.py
@@ -3,6 +3,11 @@ NLP module for sentiment and hate speech analysis using pysentimiento.
 """
 
 from pysentimiento import create_analyzer
+from transformers import pipeline
 
 sentiment_analyzer = create_analyzer(task="sentiment", lang="es")
 hate_analyzer = create_analyzer(task="hate_speech", lang="es")
+
+danger_analyzer = pipeline(
+    "text-classification", model="byandrev/evd", tokenizer="byandrev/evd"
+)
